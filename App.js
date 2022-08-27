41e6e6f6f1e49394c4b029f1d672655a026c4a41
TommiVLavi <bgr912@myy.haaga-helia.fi>
2022-08-28 00:17:02 +0300

    Teht 2 suoritettu

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useContext, useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  
+  const [numb, setNumb] = useState(0);
+  const [guess, setGuess] = useState(0);
+  const [kerta, setKerta] = useState(0);
+  const [tilanne, setTilanne] = useState('Arvaa luku ykkosesta sataseen!');
+ 
+  const napsautettu = () => {
+    setKerta(kerta + 1);
+
+    setNumb(Math.floor(Math.random() * 100) + 1);
+
+    if (numb < guess) { 
+      setTilanne('Arvauksesi ' + guess + ' on liian suuri.');
+    } else if (numb > guess) {
+      setTilanne('Arvauksesi ' + guess + ' on liian pieni.');
+    } else {
+      Alert.alert("Arvasit oikein " + kerta + " arvauksissa!");
+      setKerta(0);
+      setTilanne('Arvaa luku ykkosesta sataseen!')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{tilanne}</Text>
+
+      <TextInput 
+        style={{
+          width:150,
+          borderColor: 'pink',
+          borderWidth: 2
+        }}
+
+        onChangeText={guess => setGuess(guess)}
+        value={guess}
+      />
+
+      <Button onPress={napsautettu} title="Arvaa"/>
+
       <StatusBar style="auto" />
     </View>
   );
